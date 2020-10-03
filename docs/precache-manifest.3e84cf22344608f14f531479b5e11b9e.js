fdb78bace1f492e43e8af3ff5b68bb55d76d910c
Carlos Lemos <lemosc96@gmail.com>
2020-10-03 11:53:25 -0300

    Docs agregados

---
+++ docs/precache-manifest.3e84cf22344608f14f531479b5e11b9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7c73a95533f4d5830def36cd7f847a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "441315b7261dff34c0a5",
+    "url": "/static/css/main.e4413aa9.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441315b7261dff34c0a5",
+    "url": "/static/js/main.590a100f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
